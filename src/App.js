2ade32833bdf5af1bbc4aa1164608a19293a9fc2
Steve Medrano <60121429+darknight101-pixel@users.noreply.github.com>
2020-08-11 21:26:00 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Cards from "./components/Card";
+import Grid from "@material-ui/core/Grid";
 
-function App() {
+export default function App() {
+  let [data, setData] = useState("");
+  const access_token = "H6ZZs9MqYEiflfwHf0a8";
+  useEffect(() => {
+    fetch("https://the-one-api.herokuapp.com/v1/character", {
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + access_token,
+      },
+      method: "GET",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((Data) => {
+        setData(Data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lord of the Rings</h2>
+      {/* <Grid>
+        {data.map((el) => (
+          <Grid item xs={12} sm={4} key={el.id}>
+            <Cards img={el.wikiurl} name={el.name} realm={el.realm} />
+          </Grid>
+        ))}
+      </Grid> */}
     </div>
   );
 }
-
-export default App;
